9817f405eea72e6683a237096581ceea9e20a286
DArkDEVil <nomorese@proton.me>
2025-02-26 22:39:22 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
